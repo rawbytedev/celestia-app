58968732527e29fdcbd703db5cc2e85d8fe7863b
rawbytedev <95090911+rawbytedev@users.noreply.github.com>
2025-10-13 09:57:06 +0100

    the error came up redeclaration of functions

--- app/benchmarks/benchmark_pfb_trad_test.go
+++ app/benchmarks/benchmark_pfb_trad_test.go
@@ -220,29 +220,3 @@ func TradbenchmarkProcessProposalPFB(b *testing.B, count, size int) {
 	}
 }
 
-// generatePayForBlobTransactions creates a test app then generates a number
-// of valid PFB transactions.
-func generatePayForBlobTransactions(b *testing.B, count, size int) (*app.App, [][]byte) {
-	account := "test"
-	testApp, kr := testutil.SetupTestAppWithGenesisValSetAndMaxSquareSize(app.DefaultConsensusParams(), 128, account)
-	addr := testfactory.GetAddress(kr, account)
-	enc := encoding.MakeConfig(app.ModuleEncodingRegisters...)
-	acc := testutil.DirectQueryAccount(testApp, addr)
-	accountSequence := acc.GetSequence()
-	signer, err := user.NewSigner(kr, enc.TxConfig, testutil.ChainID, user.NewAccount(account, acc.GetAccountNumber(), acc.GetSequence()))
-	require.NoError(b, err)
-
-	rawTxs := make([][]byte, 0, count)
-	randomBytes := crypto.CRandBytes(size)
-	blob, err := share.NewBlob(share.RandomNamespace(), randomBytes, 1, acc.GetAddress().Bytes())
-	require.NoError(b, err)
-	for i := 0; i < count; i++ {
-		tx, _, err := signer.CreatePayForBlobs(account, []*share.Blob{blob}, user.SetGasLimit(2549760000), user.SetFee(10000))
-		require.NoError(b, err)
-		rawTxs = append(rawTxs, tx)
-		accountSequence++
-		err = signer.SetSequence(account, accountSequence)
-		require.NoError(b, err)
-	}
-	return testApp, rawTxs
-}
