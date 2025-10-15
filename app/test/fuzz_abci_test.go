6defc4e392419d5f7a668f2d667688c792c8e490
Aren <95090911+rawbytedev@users.noreply.github.com>
2025-10-15 21:16:38 +0100

    Update fuzz_abci_test.go

--- app/test/fuzz_abci_test.go
+++ app/test/fuzz_abci_test.go
@@ -193,9 +193,8 @@ func TestPrepareProposalInclusion(t *testing.T) {
 	}
 	tests := []test{
     // list of test for Inclusion
-    {"one hundred normal sized single blob transactions", 100, 1, 400000, 1},
 	{"many single share multi-blob transactions", 1000, 100, 400, 1},
-	{"one hundred normal sized multi-blob transactions", 100, 4, 400000, 1},
+	{"many small single share single blob transactions", 1000, 1, 400, 1},
 }
 
 
